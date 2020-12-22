a29999a9441ab30f922b75e3d3e7a66d32dc688f
Nikolaos Pispas <nikolaos.pispas@key-card.com>
2020-12-22 23:05:59 +0200

    Refactored the controller in order to follow with Laravel's naming conventions (UsersController => UserController).

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,10 +7,15 @@ use App\Models\User;
 use App\Http\Resources\User as UserResource;
 use Illuminate\Http\Request;
 
-class UsersController extends Controller
+class UserController extends Controller
 {
     public function index()
     {
         return UserResource::collection(User::all());
+    }
+
+    public function show(User $user)
+    {
+        return UserResource::make($user);
     }
 }
