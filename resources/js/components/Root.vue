65ca2a7a8b6a2ede852836b9d36c15338f9ea26e
Nikolaos Pispas <nikolaos.pispas@key-card.com>
2020-12-31 22:09:48 +0200

    Added the application's Root component. Added the necessary root components for the main models (Reservations, Rooms and Users). Added the 404 error page.

---
+++ resources/js/components/Root.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <spinner/>
+
+        <div class="db-wrapper">
+            <header-section/>
+
+            <!-- content start -->
+            <div class="db-content">
+                <div class="container-fluid">
+                    <div class="row">
+
+                        <side-nav/>
+
+                        <router-view/>
+
+                    </div>
+                </div>
+            </div>
+            <!-- content close -->
+
+            <footer-section/>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import HeaderSection from "./Sections/HeaderSection";
+import FooterSection from "./Sections/FooterSection";
+import SideNav from "./Navigation/SideNav";
+
+export default {
+    name: "Root",
+    components: {FooterSection, HeaderSection, SideNav},
+}
+</script>
---
+++ resources/js/pages/Errors/Error404.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- error start -->
+    <div class="error-page">
+        <div class="container">
+            <div class="row">
+                <div class="offset-xl-2 col-xl-8 col-md-12 col-sm-12 col-12">
+                    <div class="error-page-img">
+                        <img src="../../../images/error-img.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="error-page-content">
+                        <h1 class="error-page-content-title">Sorry. The page you are looking for does not exist</h1>
+                        <p class="error-page-content-text">We may have removed a page to which you found a link, or you may have the wrong address for the page you are looking for.
+                            Recommended actions</p>
+                        <router-link to="/overview" class="btn btn-primary">Back to homepage</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- error close -->
+</template>
+
+<script>
+    export default {
+        name: "Error404"
+    }
+</script>
