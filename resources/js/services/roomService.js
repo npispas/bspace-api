11e0fc7dce8878f30ddf75e7bcc8fbcd9810fae8
Nikolaos Pispas <npispas@gmail.com>
2021-01-11 22:43:48 +0200

    Added a service to handle room CRUD operations. Added a room type service to retrieve the available room types.

---
+++ resources/js/services/roomService.js
@@ -0,0 +1,54 @@
+import axios from "axios"
+
+class RoomService {
+     async fetchRooms() {
+        return await axios.get('api/rooms')
+            .then(response => {
+                return response.data.data
+            }).catch( error => {
+                console.error(error.message)
+            })
+    }
+
+     async deleteRoom(roomId) {
+        return await axios.delete(`api/rooms/${roomId}`)
+            .then(response => {
+                return response.data.data
+            }).catch( error => {
+                console.error(error.message)
+            })
+    }
+
+     async fetchRoom(roomId) {
+        return await axios.get(`api/rooms/${roomId}`)
+            .then( response => {
+                return response.data.data
+            }).catch( error => {
+                console.error(error.message)
+            })
+    }
+
+     async createRoom(data) {
+        return await axios.post('api/rooms', {
+            name: data.name,
+            location: data.location,
+            interior_size: data.interiorSize,
+            room_type_id: data.roomType,
+            min_occupancy: data.minOccupancy,
+            max_occupancy: data.maxOccupancy,
+            available_from: data.availableFrom,
+            available_to: data.availableTo,
+            is_published: data.isPublished,
+            description: data.description,
+            images: data.images
+        })
+            .then( response => {
+                return response.data.data
+            }).catch( error => {
+                console.error(error.message)
+            })
+    }
+}
+
+export default new RoomService()
+
---
+++ resources/js/services/roomTypeService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+class RoomTypeService {
+     async fetchRoomTypes() {
+        return await axios.get('api/room_types')
+            .then(response => {
+                return response.data.data
+            }).catch( error => {
+                console.error(error.message)
+            })
+    }
+
+     async fetchRoomType(roomTypeId) {
+        return await axios.get(`api/room_types/${roomTypeId}`)
+            .then( response => {
+                return response.data.data
+            }).catch(error => {
+                console.error(error.message)
+            })
+    }
+}
+
+export default new RoomTypeService()
+
