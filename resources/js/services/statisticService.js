5ce2b40319522696e27e945013066e15375e533a
Nikolaos Pispas <npispas@gmail.com>
2021-01-11 23:29:14 +0200

    Removed left over logger.

--- resources/js/services/statisticService.js
+++ resources/js/services/statisticService.js
@@ -4,7 +4,6 @@ class StatisticService {
     async fetchStatistics() {
         return await axios.get('api/statistics')
             .then(response => {
-                console.log(response.data.data)
                 return response.data.data
             }).catch( error => {
                 console.error(error.message)
