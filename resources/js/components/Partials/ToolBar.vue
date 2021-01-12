20df17cf8bba402eb3de9697e85cf36704b3d673
Nikolaos Pispas <npispas@gmail.com>
2021-01-12 20:36:33 +0200

    Added an avatar and a title to show the authenticated user's username.

--- resources/js/components/Partials/ToolBar.vue
+++ resources/js/components/Partials/ToolBar.vue
@@ -6,18 +6,33 @@
        dark
     >
         <v-toolbar-title>B-Space</v-toolbar-title>
-
         <v-spacer></v-spacer>
-
+        <v-toolbar-title class="text-subtitle-1">{{ username }}</v-toolbar-title>
         <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
+            <v-avatar size="36px">
+                <v-img src="../../../images/avatar-1.jpg"></v-img>
+            </v-avatar>
         </v-btn>
     </v-app-bar>
 </template>
 
 <script>
+import AuthService from "../../services/authService";
+
 export default {
     name: "ToolBar",
+
+    created() {
+        AuthService.fetchAuthUser().then(response => {
+            this.username = response.username
+        })
+    },
+
+    data() {
+        return {
+            username: ''
+        }
+    },
 
     computed: {
         mini() {
