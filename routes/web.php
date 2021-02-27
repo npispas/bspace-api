472672690925a4ddd348104432dd1e172bd39cbe
Nikolaos Pispas <npispas@gmail.com>
2021-02-27 17:04:56 +0200

    Api routes and Cors.
    - Added the missing api routes for availability and checkin.
    - Added the missing web routes for registration, verification and resend verification.
    - Added registration to the Cors list.

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login'])
     ->middleware('guest');
 
+Route::post('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'register'])
+    ->middleware('guest');
+
+Route::get('email/verify/{id}', [\App\Http\Controllers\Auth\VerificationController::class, 'verify'])
+    ->name('verification.verify');
+
+Route::get('email/resend', [\App\Http\Controllers\Auth\VerificationController::class, 'resend'])
+    ->name('verification.resend');
+
 Route::post('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout']);
